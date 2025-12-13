7fee8cee773e6e298d316d1a707a401309d59ff7
codewithmanohar <mksahni2555@gmail.com>
2025-12-13 20:11:41 +0530

    commit : updated config file

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,32 @@
 import mongoose from "mongoose";
-// connection 
-const connectDB = async (req,res) =>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Civic");
-        console.log(`Mongodb Connected : ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error : ${error.message}`);
-    }
+import dotenv from "dotenv"
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("❌ MONGODB_URI is not defined");
 }
 
+let cached = global.mongoose;
 
-export default connectDB ;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default connectDB;
