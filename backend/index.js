60e3f2611bf809b2cfdd4eeb43cd3e08970c9f05
codewithmanohar <mksahni2555@gmail.com>
2025-12-14 18:58:41 +0530

    commit : changed frontend origin url

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://civixs.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
